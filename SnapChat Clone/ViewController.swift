8b537ff1deeec2574ea849fd3866bb7b5cea6836
xecxecikk <xecekrdnz_94@outlook.com>
2025-05-09 15:37:30 +0300

    Initial Commit

---
+++ SnapChat Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapChat Clone
+//
+//  Created by XECE on 9.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
